8de40c9e14009358a11e6546cefb272749f8130d
mTy8421 <koontyza007@gmail.com>
2022-12-14 23:47:36 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CompanyCRUDController;
 
+Route::resource('companies', CompanyCRUDController::class);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
