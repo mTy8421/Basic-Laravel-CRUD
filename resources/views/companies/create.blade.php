3d305b1a1a9eb085c92b8bce8c3506eceae936c4
mTy8421 <koontyza007@gmail.com>
2022-12-15 00:00:22 +0700

    update create

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -9,6 +9,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2> Add Company</h2>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
