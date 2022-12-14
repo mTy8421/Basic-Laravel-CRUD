70ee0399eb45fef6ede915942ce4a8080ad58dbb
mTy8421 <koontyza007@gmail.com>
2022-12-14 23:40:17 +0700

    update controller

--- app/Http/Controllers/CompanyCRUDController.php
+++ app/Http/Controllers/CompanyCRUDController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Company;
+
 class CompanyCRUDController extends Controller
 {
     //  create Index
+    public function index(){
+        $data['companies'] = Company::orderBy('id', 'desc')->paginate(5);
+        return view('companies.index', $data);
+    }
+
+    // create resource
+    public function create(){
+        return view('companies.create');
+    }
 }
